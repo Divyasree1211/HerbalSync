72dc86a34da38fd8a9a27942af739eae93741e74
Divyasree1211 <divyasreemaddala2@gmail.com>
2026-05-22 13:45:37 +0530

    Simplify CORS middleware configuration
    Refactor CORS middleware setup for clarity and consistency.

--- Server/server.js
+++ Server/server.js
@@ -8,38 +8,40 @@ const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const nutritionRoutes = require("./routes/nutritionRoutes");
 
+// Load environment variables
 dotenv.config();
 
+// Connect Database
 connectDB();
 
 const app = express();
 
-const cors = require("cors");
+// Middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://herbal-sync-git-main-divya12.vercel.app",
+      "https://herbal-sync-j3hwdhrg9-divya12.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://herbal-sync-git-main-divya12.vercel.app",
-    "https://herbal-sync-j3hwdhrg9-divya12.vercel.app"
-  ],
-  credentials: true
-}));
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
-
 app.use("/api/products", productRoutes);
 app.use("/api/nutrition", nutritionRoutes);
 
+// Default Route
 app.get("/", (req, res) => {
-  res.send("HerbalSync API Running");
+  res.send("HerbalSync API Running 🚀");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("API Running Successfully 🚀");
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
